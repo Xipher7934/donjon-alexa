8dde294bb97b0f7cf09482c1257c825db5a6abc9
Ethan Heming <heming.e@gmail.com>
2018-07-22 12:07:51 -0400

    Initialize

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,64 @@
 class ApplicationController < ActionController::API
 
   def netflix
-    system('start chrome netflix.com')
+    show_code = "/search?q=#{params[:show]}" if params[:show]
+    system("start chrome netflix.com#{show_code}")
     render json: true, status: :ok
+  end
+
+  def netflixcontrol
+    require 'win32ole'
+    control = case [:control]
+              when 'pause', 'resume'
+                '{SPACE}'
+              when 'full screen'
+                'F'
+              when 'go back'
+                '{LEFT}'
+              when 'go forward'
+                '{RIGHT}'
+              when 'mute'
+                '{M}'
+              else
+                ''
+              end
+    wsh = WIN32OLE.new('Wscript.Shell')
+    wsh.SendKeys(control)
+    render json: { response: true }, status: :ok
+  end
+
+  def bootup
+    require 'win32ole'
+    wsh = WIN32OLE.new('Wscript.Shell')
+    wsh.SendKeys("^{ESC}")
+    sleep(0.5)
+    wsh.SendKeys(params[:program])
+    sleep(0.5)
+    wsh.SendKeys("{ENTER}")
+    render json: { response: true }, status: :ok
+  end
+
+  def closeprogram
+    require 'win32ole'
+    if params[:program]
+      if system('taskkill /f /fi "IMAGENAME eq ' + params[:program] + '*" /im *')
+        render json: { response: 'Ok' }, status: :ok
+      else
+        render json: { response: 'Error' }, status: :unprocessable_entity
+      end
+    else
+      wsh = WIN32OLE.new('Wscript.Shell')
+      wsh.SendKeys("%{F4}")
+      render json: { response: true }, status: :ok
+    end
+  end
+
+  def shutdown
+    system('shutdown')
+  end
+
+  def restart
+    system('restart')
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get 'netflix', to: 'application#netflix'
+  post 'netflix', to: 'application#netflix'
+
+  post 'netflixcontrol', to: 'application#netflixcontrol'
+
+  post 'bootup', to: 'application#bootup'
+
+  post 'closeprogram', to: 'application#closeprogram'
+
+  post 'shutdown', to: 'application#shutdown'
+
+  post 'restart', to: 'application#restart'
 end
