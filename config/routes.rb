0d6be6867d56bebacf988aacd211e70240fafe86
Ethan Heming <heming.e@gmail.com>
2018-07-24 11:29:35 -0400

    Initial

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get 'monsterlookup', to: 'application#monsterlookup'
+  post 'monsterlookup', to: 'application#monsterlookup'
 
 end
