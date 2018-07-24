b3908b441120b6cc31bac7223ae6d9c656d46f0f
Ethan Heming <heming.e@gmail.com>
2018-07-24 12:12:16 -0400

    Initial

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -58,7 +58,7 @@ class ApplicationController < ActionController::API
 
     mons = parse_monster(monster_text[:card])
 
-    if params[:request][:intent][:name] = "monster_lookup"
+    if params[:request][:intent][:name] == "monster_lookup"
       return_text = "The #{mons[:name]} is #{mons[:description].with_indefinite_article}. It is CR #{mons[:challenge]}"
     else
       stat = params[:request][:intent][:slots][:stat][:value]
